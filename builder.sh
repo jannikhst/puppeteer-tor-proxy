dac974a59b8e935a947695d06d14f571e993600b
jannikhst <58545820+jannikhst@users.noreply.github.com>
2023-07-07 16:01:37 +0200

    updated builder

--- builder.sh
+++ builder.sh
@@ -1,9 +1,15 @@
 git pull
+
+./stopClickers.sh
+
 # Prompt for browsers value
 read -p "Enter the number of browsers: " browsers
 
 # Prompt for workers value
 read -p "Enter the number of workers: " workers
+
+# Prompt for number of clickers
+read -p "Enter the number of clickers to start: " clickers
 
 # Update the config file
 echo "browsers=$browsers" > worker.config
@@ -11,6 +17,11 @@ echo "workers=$workers" >> worker.config
 
 echo "Config file updated successfully."
 
-
+# Build the Docker image
 docker build . -t clicker:latest
-./startClicker.sh
+
+# Start the clickers
+for ((i=1; i<=$clickers; i++))
+do
+    ./startClicker.sh
+done
